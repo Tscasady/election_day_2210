4deefa76f7beba09a9358df9be4964e8c686f030
Thomas Casady <tscasady@gmail.com>
2022-11-01 10:21:59 -0500

    feat: Add Race class.

--- lib/race.rb
+++ lib/race.rb
@@ -1,2 +1,14 @@
 class Race
+  attr_reader :office, :candidates
+
+  def initialize(office)
+    @office = office
+    @candidates = []
+  end
+
+  def register_candidate!(data)
+    candidate = Candidate.new(data)
+    @candidates << candidate
+    candidate
+  end
 end
--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -2,14 +2,15 @@ require './lib/race'
 require './lib/candidate'
 
 describe Race do
-  let(race){Race.new('Texas Governor')}
-    
+      
   describe '#initialize' do
     it 'exists' do
+      race = Race.new('Texas Governor')
       expect(race).to be_a Race
     end
 
     it 'has readable attributes' do
+      race = Race.new('Texas Governor')
       expect(race.office).to eq "Texas Governor"
       expect(race.candidates).to eq []
     end
@@ -17,6 +18,7 @@ describe Race do
 
   describe '#register_candidate!' do
     it 'creates a new candidate and adds it to the Race candidates array' do
+      race = Race.new('Texas Governor')
       expect(race.candidates).to eq []
       candidate1 = race.register_candidate!({name: 'Diana D', party: :democrat})
       candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
