efd728c3a4c10c9951696271333a916f617fd2f1
Thomas Casady <tscasady@gmail.com>
2022-11-01 10:57:59 -0500

    test: Add winner, close, open?, and tie? tests to race.

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -25,4 +25,57 @@ describe Race do
       expect(race.candidates).to eq [candidate1, candidate2]
     end
   end
+
+  descirbe '#open?' do
+    it 'returns a boolean indicating whether the race is open or not' do
+      race = Race.new('Texas Governor')
+      expect(race.open?).to be true
+      race.close!
+      expect(race.open?).to be false
+    end
+  end
+
+  descirbe 'close!' do
+    it 'closes the race' do
+      race = Race.new('Texas Governor')
+      expect(race.open?).to be true
+      race.close!
+      expect(race.open?).to be false
+    end
+  end
+
+  descirbe 'winner' do
+    it 'returns false if the race is open, otherwise returns candidate with the most votes' do
+      race = Race.new('Texas Governor')
+      candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+      4.times {candidate1.vote_for!}
+      1.times {candidate2.vote_for!}
+      expect(race.winner).to eq false
+      race.close!
+      expect(race.winner).to eq 'Diana D'
+    end
+
+    it 'can return any of the highest voted candidates in case of a tie' do
+      race = Race.new('Texas Governor')
+      candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+      4.times {candidate1.vote_for!}
+      4.times {candidate2.vote_for!}
+      race.close!
+      expect(race.winner).to eq 'Diana D'
+    end
+  end
+
+  descirbe 'tie?' do
+    it 'returns a boolean indicating whether or not two or more candidates received the highest number of votes' do
+      race = Race.new('Texas Governor')
+      candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+      4.times {candidate1.vote_for!}
+      4.times {candidate2.vote_for!}
+      race.close!
+      expect(race.tie?).to eq true
+    end
+  end
 end
